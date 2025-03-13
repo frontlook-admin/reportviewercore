37ff946767b2abec2dc994e48eca296ab76fc208
FrontLook-Admin <frontlook.inc@gmail.com>
2025-03-13 12:28:00 +0530

    chore: Update project references and clean up code
    Modified several .csproj files to add new project references for
    `FrontLookUpdater.Lib` and `FrontLookCoreLibraryAssembly.Hosting.DbContext`.
    Removed commented-out package references and adjusted item group
    structures.
    
    In `CustomPrintDialog.cs`, added a new using directive, refined
    the `MarginConversion` calculation, and updated method calls for
    better clarity and consistency.

--- Microsoft.ReportViewer.WinForms/FrontLookCode/CustomPrintDialog.cs
+++ Microsoft.ReportViewer.WinForms/FrontLookCode/CustomPrintDialog.cs
@@ -9,6 +9,7 @@ using System.Windows.Forms;
 using System.IO;
 using Microsoft.Reporting.WinForms;
 using System.Text.Json.Serialization;
+using Microsoft.ReportViewer.Common.FrontLookCode;
 
 namespace Microsoft.ReportViewer.WinForms.FrontLookCode
 {
@@ -40,7 +41,7 @@ namespace Microsoft.ReportViewer.WinForms.FrontLookCode
         //public bool? MetricEnabled { get; set; }
         public PaperSize PaperSize { get; set; }
         //private static double MarginConversion = 0.394;
-        private static double MarginConversion = (1/2.54);
+        private static double MarginConversion = (1 / 2.54);
         public Margins Margins { get; set; }
 
         //[JsonIgnore]
@@ -83,8 +84,9 @@ namespace Microsoft.ReportViewer.WinForms.FrontLookCode
 
                 return m;
             }
-            catch { 
-                return Margins; 
+            catch
+            {
+                return Margins;
             }
         }
 
@@ -205,7 +207,7 @@ namespace Microsoft.ReportViewer.WinForms.FrontLookCode
 
         public CustomPrintDialog(string JsonData)
         {
-            var printDialog = JsonData.FL_CastToClass<CustomPrintDialog>();
+            var printDialog = JsonData.CastToClass<CustomPrintDialog>();
             PrinterName = printDialog.PrinterName;
             AllowSomePages = printDialog.AllowSomePages;
             AllowSelection = printDialog.AllowSelection;
@@ -240,7 +242,7 @@ namespace Microsoft.ReportViewer.WinForms.FrontLookCode
 
         public virtual string GetJsonData()
         {
-            return this.FL_CastToJson();
+            return this.CastToJson();
         }
 
         public virtual PrinterSettings GetPrinterSettings()
@@ -291,7 +293,7 @@ namespace Microsoft.ReportViewer.WinForms.FrontLookCode
                 }
                 else
                 {
-                    if(CPageSettings == null)
+                    if (CPageSettings == null)
                     {
 
                         if (PaperSize == null)
